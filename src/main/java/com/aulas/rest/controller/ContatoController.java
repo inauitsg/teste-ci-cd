e9a3e78de00564ad0937f137e05c82aeb1762c2b
inauitsg <inauitsg@gmail.com>
2022-06-10 09:19:28 -0300

    projeto criado

---
+++ src/main/java/com/aulas/rest/controller/ContatoController.java
@@ -0,0 +1,18 @@
+package com.aulas.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contato")
+public class ContatoController {
+	
+	@GetMapping
+	public ResponseEntity<String> ola(){
+		return ResponseEntity.status(HttpStatus.OK).body("Hello world");
+	}
+
+}
